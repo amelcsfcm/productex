4afa45f1f83ac1020d76900d3c32cc77dc89a0fe
5h140w <5h140w23@gmail.com>
2023-09-18 20:34:53 +0100

    PULL REQUEST

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
-const express=require("express")
-const productrouter = require("./routers/productrouters")
-require("./config/productdb").connect()
-const cors=require("cors")
-const app=express()
-app.use(express.json())
-app.use(cors())
-app.use(productrouter)
-app.listen(8080,()=>{console.log("listening to port 8080")})
+const express = require("express");
+const productrouter = require("./routers/productrouters");
+require("./config/productdb").connect();
+const cors = require("cors");
+const Authrouter = require("./routers/authrouters");
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(productrouter);
+app.use(Authrouter);
+
+app.listen(8080, () => {
+  console.log("listening to port 8080");
+});
--- backend/model/product.js
+++ backend/model/product.js
@@ -1,10 +1,8 @@
-const mongoose=require("mongoose")
-const proschema= new mongoose.Schema({
-    title:{type:String, required:true, unique:true},
-    description:{type:String, required:true},
-    price:{type:Number, default:10}
-})
+const mongoose = require("mongoose");
+const proschema = new mongoose.Schema({
+  title: { type: String, required: true, unique: true },
+  description: { type: String, required: true },
+  price: { type: Number, default: 10 },
+});
 
-module.exports=mongoose.model("product", proschema)
-    
-
+module.exports = mongoose.model("product", proschema);
